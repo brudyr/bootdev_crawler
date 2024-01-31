d1ef218d46e3a351ef7150560eb36a7fe85dfd96
brudyr <brudyr@haufe.com>
2024-01-31 15:16:47 +0100

    implement crawling and report

--- crawl.js
+++ crawl.js
@@ -5,7 +5,7 @@ const { JSDOM } = require("jsdom");
 function normalizeURL(urlString) {
   try {
     const url = new URL(urlString)
-    return `${url.host}${removeTrailingSlash(url.pathname)}${url.search}${url.hash}`
+    return `https://${url.host}${removeTrailingSlash(url.pathname)}${url.search}${url.hash}`
   } catch (e) {
     console.log("The entered string is not a valid url")
     return null
@@ -18,13 +18,88 @@ function getURLsFromHTML(htmlBody, baseURL) {
   const linkNodes = dom.window.document.querySelectorAll("a");
   const result = [];
   linkNodes.forEach( node => {
-    result.push(`${baseURL}${node.href}`);
+    result.push(`${node.href[0] === "/" ? baseURL : ""}${node.href}`);
   })
 
   return result
 }
 
+async function crawlPage(baseURL, currentURL, pages) {
+  if (!currentURL) {
+    currentURL = normalizeURL(baseURL)
+  }
+
+  if (!pages) {
+    pages = {}
+  }
+
+  baseURL = normalizeURL(baseURL)
+  currentURL = normalizeURL(currentURL)
+
+
+  if ((new URL(baseURL)).host !== (new URL(currentURL)).host) {
+    // console.log("Got an external URL. Skipping...")
+    return pages
+  }
+
+  const normalizedURL = normalizeURL(currentURL)
+
+  if (pages[normalizedURL] !== undefined) {
+    pages[normalizedURL]++
+  } else {
+    if (normalizedURL === normalizeURL(baseURL)) {
+      pages[normalizedURL] = 0
+    } else {
+      pages[normalizedURL] = 1
+    }
+
+    try {
+      const response = await fetch(currentURL)
+      if (response.status > 400) {
+        console.log("Request was unsuccessful")
+        return pages
+      }
+      if (response.headers.get("content-type") !== "text/html; charset=utf-8") {
+        // console.log(`ContentType was not text/html. Got: '${response.headers.get("content-type")}'`)
+        return pages
+      }
+
+      const html = await response.text();
+      const links = getURLsFromHTML(html, baseURL);
+
+      for (let i = 0; i < links.length; i++) {
+        pages = await crawlPage(baseURL, links[i], pages)
+      }
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+  return pages
+}
+
+
+function getSortedPagesKeys(pages) {
+  var keys = Object.keys(pages);
+
+  return keys.sort(function(a,b){
+    return pages[b]-pages[a];
+  });
+}
+
+function printReport(pages) {
+
+  var keys = getSortedPagesKeys(pages);
+
+  console.log("Report:");
+  for (let i = 0; i < keys.length; i++) {
+    console.log(`Found ${pages[keys[i]]} internal links to ${keys[i]}`)
+  }
+
+}
+
 module.exports = {
   normalizeURL,
-  getURLsFromHTML
+  getURLsFromHTML,
+  crawlPage,
+  printReport
 }
--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const { getURLsFromHTML } = require("./crawl")
+const { crawlPage, printReport } = require("./crawl")
 
-function main() {
+async function main() {
   if (process.argv.length !== 3) {
     console.log("The programm expects exactly one argument: 'npm start BASE_URL'");
+    return
   }
-  console.log("Doing Stuff")
+  console.log("Starting crawl...")
+  const pages = await crawlPage("https://wagslane.dev")
+  console.log("Crawl finished. Printing report...")
+  printReport(pages)
 
 }
 
